5ca8cdf0cc7fc136a3831d79ab6e5b7ebcfd6b7a
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:15:17 -0500

    solution exercise 15

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-letraSecreta = "a"
-
-while True:
-    desicion = input("Enter a letter between A and E: ").lower()
-
-    if desicion not in ["a", "b", "c", "d", "e"]:
-        print("Please enter a valid letter between A and E.")
-        continue
-
-    def guess(resultado):
-        match resultado:
-            case "b":
-                return "Nah bro B that is not the correct letter."
-            case "c":
-                return "Nah bro C that is not the correct letter."
-            case "d":
-                return "Nah bro D that is not the correct letter."
-            case "e":
-                return "Nah bro E that is not the correct letter."
-
-    respuesta = guess(desicion)
-    print(respuesta)
-
-    if desicion == letraSecreta:
-        print("WOWWWW YOU GUESSED IT!")
-        break
+salary=float(input("put your salary: "))
+country=input("select your country write: 'Pais A' 'Pais B' 'Pais C' 'Pais D':\n")
+if country.lower()=="pais a":
+    taxes=salary-(0.20*salary)
+    print(f"your salary with taxes is {taxes}")
+elif country.lower()=="pais b":
+    taxes=salary-(0.15*salary)
+    print(f"your salary with taxes is {taxes}")
+elif country.lower()=="pais c":
+    taxes=salary-(0.10*salary)
+    print(f"your salary with taxes is {taxes}")
