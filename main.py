9dc7875cd919e4f531a845136b896d75b646a9d3
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:50:26 -0500

    solution exercise iterativa(3)

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-cadena = input("Ingresa una cadena de texto: ")
-vocales = "aeiou"
-contador = 0
+n = int(input("Ingresa un número entero positivo: "))
 
-for letra in cadena:
-    if letra.lower() in vocales:
-        contador += 1
-
-print(f"La cantidad de vocales en la cadena es: {contador}")
+if n < 0:
+    print("Por favor, ingresa un número entero positivo.")
+else:
+    factorial = 1
+for i in range(1, n + 1):
+    factorial *= i
+    print(f"El factorial de {n} es: {factorial}")
