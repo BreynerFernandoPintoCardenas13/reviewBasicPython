3dc1000f2923380c11a60d1c1b1c857bb669029c
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:12:39 -0500

    solution exercise 6

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-day=int(input("enter a number from 1 to 7: "))
-if day<7 or day>1:
-    if day==1:
-        print("monday")    
-    elif day==2:
-        print("thuesday")
-    elif day==3:
-        print("wednesday")
-    elif day==4:
-        print("thursday")
-    elif day==5:
-        print("friday")
-    elif day==6:
-        print("saturday")
-    elif day==7:
-        print("sunday")
-else:
-    print("try again")
+import random
+print("im think in a random number, guess")
+numberTwo=0
+number=random.randint(1, 10)
+while numberTwo!=number:
+
+    numberTwo=int(input("enter the number: "))
+    if numberTwo<number:
+        print("the number is more higher")
+    elif numberTwo>number:
+        print("the number is more lower")
+
+print(f"you guess the number was {number}")
