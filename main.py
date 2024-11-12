dfdd1a53511f6b5a93a244cb86a1669c80c19691
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:09:55 -0500

    solution exercise 3

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-nota=float(input("ENter the grade: "))
-if nota>60:
-    print("your grade its best then 60 Happy for you, your pass")
-else:
-    print("Your grade sucks its less then 60 Oh men you lost the grade")
+exit=1
+while exit:
+    number=float(input("enter a number: ")) 
+    numberTwo=float(input("enter a number: "))
+    operator=input("enter a operator: ")
+    if operator=="+":
+        plus=int(number+numberTwo)
+        print(f"the rest is {plus}")
+    elif operator=="-":
+        plus=int(number-numberTwo)
+        print(f"the rest is {plus}")
+    elif operator=="*":
+        plus=number*numberTwo
+        print(f"the rest is {plus}")
+    elif operator=="/":
+        plus=number/numberTwo
+        print(f"the rest is {plus}")
+    else:
+        print("no valido")
+
+    exit=int(input(f"if yo want continue enter [1]\nif you dont want enter[0]\n"))
+    
