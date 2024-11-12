01d8e2ea07f4eaee9121174324b1a8483f00085c
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:13:38 -0500

    solution exerice 9

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-year=int(input("enter a year:"))
-if year%4==0 or year%400==0: 
-    print("your year is leap")
-else:
-    print("your year is not leap")
+age=int(input("enter your age: "))
+if age<=12:
+    print("you are a child")
+elif age>12 and age<=17:
+    print("you are a teenager")
+elif age>17 and age<=64:
+    print("you are a adult")
+elif age>64 and age<=119:
+    print("you are a old man")
+elif age>120:
+    print("yor fuckin dead men")
