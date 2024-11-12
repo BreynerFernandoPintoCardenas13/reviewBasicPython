b2be7fd23a8306f2a1391ce8a15ad7ab383420e4
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:13:57 -0500

    solution exercise 10

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-age=int(input("enter your age: "))
-if age<=12:
-    print("you are a child")
-elif age>12 and age<=17:
-    print("you are a teenager")
-elif age>17 and age<=64:
-    print("you are a adult")
-elif age>64 and age<=119:
-    print("you are a old man")
-elif age>120:
-    print("yor fuckin dead men")
+numericalNote=input("enter a numerical note: ")
+if numericalNote.lower()=="a":
+    print("you are a fucking best you grade is betwen 100-90")
+elif numericalNote.lower()=="b":
+    print("very nice bro you grade is betwen 80-89")
+elif numericalNote.lower()=="c":
+    print("you can do better you grade is betwen 70-79")
+elif numericalNote.lower()=="d":
+    print("men you have to improve you grade is betwen 60-69")
+elif numericalNote.lower()=="f":
+    print("Bruh your grade its more lower then 60, better go to work")
