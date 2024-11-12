bd41272957b16256ba8e3dad5448ea5d021500c5
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:14:30 -0500

    solution exercise 12

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-desicion=int(input(f"if you want convert the temperature to celsius enter [1]\nif you want convert to farenheit[2]\n"))
-def grades(lista):
-    match lista:
-        case [1]:
-            gradesIn=float(input("enter a temperature in farhenheit:"))
-            conversion=(gradesIn-32)*(5/9)
-            return f"the conversion is: {conversion}"
-        case [2]:
-            gradesIn=float(input("enter a temperature in celsius:"))
-            conversion=gradesIn*(9/5)+32
-            return f"the conversion is:{conversion}"
-
-print(grades([desicion]))  # Salida: "Lista con tres elementos: 1, 2, 3"
+height=float(input("enter you height in meters: "))
+weight=float(input("enter you weight in kg: "))
+imc= weight/height**2
+if imc<18.5:
+    print("you underweight")
+elif imc>18.5 and imc<=24.9:
+    print("you weight its normal")
+elif imc>18.5 and imc<24.9:
+    print("you weight its normal")
+elif imc>24.9 and imc<29.9:
+    print("you weight its high weight")
+elif imc>=30:
+    print("you weight its overweight")
+else:
+    print("try again")
