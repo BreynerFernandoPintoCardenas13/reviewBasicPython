83e41e7fe165965a81bbdd8d0a1b317d85983e45
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:10:59 -0500

    solution exercise 5

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-i=1
-sideOne=float(input(f"enter the side{i}"))
-sideTwo=float(input(f"enter the side{i}"))
-sideThree=float(input(f"enter the side{i}"))
-if sideOne == sideTwo == sideThree:
-    print("the triangle is equilatero")
-if sideOne != sideTwo != sideThree:
-    print("the triangle is escaleno")
-if sideOne != sideTwo == sideThree or sideOne == sideTwo != sideThree:
-    print("the triangle is isoceles")
+day=int(input("enter a number from 1 to 7: "))
+if day<7 or day>1:
+    if day==1:
+        print("monday")    
+    elif day==2:
+        print("thuesday")
+    elif day==3:
+        print("wednesday")
+    elif day==4:
+        print("thursday")
+    elif day==5:
+        print("friday")
+    elif day==6:
+        print("saturday")
+    elif day==7:
+        print("sunday")
+else:
+    print("try again")
