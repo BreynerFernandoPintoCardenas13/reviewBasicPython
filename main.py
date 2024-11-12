d49969ac79b87fa44cb9b9a161e6372cca6beea2
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:15:01 -0500

    solution exercise 14

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-number1=float(input("enter a number: "))
-number2=float(input("enter a number: "))
-number3=float(input("enter a number: "))
-if number1>number2>=number3:
-    print(f"the more higher is {number1}")
-elif number2>number1>=number3:
-    print(f"the more higher is {number2}")
-else:  
-    number3>number1>=number2
-    print(f"the more higher is {number3}")
+letraSecreta = "a"
+
+while True:
+    desicion = input("Enter a letter between A and E: ").lower()
+
+    if desicion not in ["a", "b", "c", "d", "e"]:
+        print("Please enter a valid letter between A and E.")
+        continue
+
+    def guess(resultado):
+        match resultado:
+            case "b":
+                return "Nah bro B that is not the correct letter."
+            case "c":
+                return "Nah bro C that is not the correct letter."
+            case "d":
+                return "Nah bro D that is not the correct letter."
+            case "e":
+                return "Nah bro E that is not the correct letter."
+
+    respuesta = guess(desicion)
+    print(respuesta)
+
+    if desicion == letraSecreta:
+        print("WOWWWW YOU GUESSED IT!")
+        break
