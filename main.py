a356fce83925d0ff656b297b51600ec888592865
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:10:37 -0500

    solution exercise 4

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-exit=1
-while exit:
-    number=float(input("enter a number: ")) 
-    numberTwo=float(input("enter a number: "))
-    operator=input("enter a operator: ")
-    if operator=="+":
-        plus=int(number+numberTwo)
-        print(f"the rest is {plus}")
-    elif operator=="-":
-        plus=int(number-numberTwo)
-        print(f"the rest is {plus}")
-    elif operator=="*":
-        plus=number*numberTwo
-        print(f"the rest is {plus}")
-    elif operator=="/":
-        plus=number/numberTwo
-        print(f"the rest is {plus}")
-    else:
-        print("no valido")
-
-    exit=int(input(f"if yo want continue enter [1]\nif you dont want enter[0]\n"))
-    
+i=1
+sideOne=float(input(f"enter the side{i}"))
+sideTwo=float(input(f"enter the side{i}"))
+sideThree=float(input(f"enter the side{i}"))
+if sideOne == sideTwo == sideThree:
+    print("the triangle is equilatero")
+if sideOne != sideTwo != sideThree:
+    print("the triangle is escaleno")
+if sideOne != sideTwo == sideThree or sideOne == sideTwo != sideThree:
+    print("the triangle is isoceles")
