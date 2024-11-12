ca0d660e9c4425e2d0b31380510efd148ddf46c4
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:15:50 -0500

    solution exercise 17

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-distance=float(input("enter the distance: "))
-kmH=float(input("enter the velocity of your have: "))
-if kmH>120:
-    print("CAREFUL YOU SPEED IS TO MUCH")
-else: 
-    time=distance/kmH
-    hours=int(time)
-    minuts=round(time-hours)*60
-    print(f"you travel is {hours}hours and {minuts}minutes")
+while True:
+    grade=float(input("enter the grade: "))
+    bonification=input("you did adicional task? Answer yes or no: ")
+    if bonification.lower()=="yes":
+        operation=((5/100)*grade)+grade
+        if operation>100:
+            print(f"Great, with the adicional task your final grade is: 100")
+            break
+        else:
+            print(f"Great, with the adicional task your final grade is: {operation}")
+            break
+    elif bonification.lower()=="no":
+        print(f"Okey you final grade is: {grade}")
+        break
+    else:
+        print("try again and follow the instructions")
+        
