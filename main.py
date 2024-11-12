0ed4325aa0567056c6f3d5cb055d1f49a94b7ad1
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-12 12:49:34 -0500

    solution exercise iterativa(2)

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-n = int(input("Ingresa un número entero positivo: "))
+cadena = input("Ingresa una cadena de texto: ")
+vocales = "aeiou"
+contador = 0
 
-if n <= 0:
-    print("Por favor, ingresa un número positivo mayor que 0.")
-else:
-    suma = 0
-for i in range(1, n + 1):
-    suma += i
-print(f"La suma de los primeros {n} números enteros es: {suma}")
+for letra in cadena:
+    if letra.lower() in vocales:
+        contador += 1
+
+print(f"La cantidad de vocales en la cadena es: {contador}")
